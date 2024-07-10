1fb566edac2ce6a1e3501393bd2ac729c070939a
DFBlok <s219392862@mandela.ac.za>
2024-07-10 11:36:00 +0200

    added CORS Policy

--- BookStoreProject-MERN/backend/index.js
+++ BookStoreProject-MERN/backend/index.js
@@ -3,11 +3,22 @@ import { PORT, monogoDB } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRouter from "./routes/booksRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 //middleware for parsing request body
 app.use(express.json());
+
+//Middware for handling CORS Policy
+//Option 2: Allow custom Orgins
+app.use(
+  cors({
+    origin: "http://loacalhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);

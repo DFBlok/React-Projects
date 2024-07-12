1e4362f98d686b5396b4235ec288e96b9be56215
DFBlok <s219392862@mandela.ac.za>
2024-07-12 13:52:07 +0200

    view options

--- BookStoreProject-MERN/backend/config.js
+++ BookStoreProject-MERN/backend/config.js
@@ -1,4 +1,5 @@
 export const PORT = 3001;
 
 export const monogoDB =
-  "mongodb+srv://root:hMusDAmJHYkIdDY9@books-store-mern.xdgc6ci.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN";
+  /*  "mongodb+srv://root:hMusDAmJHYkIdDY9@books-store-mern.xdgc6ci.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"; */
+  "mongodb+srv://DFBlok:BANpgLmpibdZbmVo@atlascluster.6bfujd2.mongodb.net/Book-Store-MERN-V2?retryWrites=true&w=majority&appName=AtlasCluster";

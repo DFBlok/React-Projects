13beb6043d6e1c2a39ac1a2ef712c589eca8a1b7
DFBlok <s219392862@mandela.ac.za>
2024-07-15 11:19:37 +0200

    Fix permission issue prt3

--- BookStoreProject-MERN/backend/config.js
+++ BookStoreProject-MERN/backend/config.js
@@ -1,4 +1,6 @@
-export const PORT = 3001;
+/* export const PORT = 3001; */
+
+export const PORT = process.env.PORT || 3001;
 
 export const monogoDB =
   /*  "mongodb+srv://root:hMusDAmJHYkIdDY9@books-store-mern.xdgc6ci.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN"; */

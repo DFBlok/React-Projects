8bdb331d450b4f32a0e6d72148aaf80f73fbc3b3
DFBlok <s219392862@mandela.ac.za>
2024-07-22 13:56:18 +0200

    Initial Project

---
+++ nextjs-ecomm/README.md
@@ -0,0 +1,3 @@
+#E-Commerce App
+
+Using Next.js, prisma, shadcn, typescript and mongoBD
---
+++ nextjs-ecomm/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page!!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+        exercitationem fuga accusamus autem aliquid et, officia incidunt eos
+        magnam corporis sit velit minima. Impedit qui, dignissimos dolorum
+        assumenda laboriosam nobis.
+      </p>
+    </main>
+  );
+}

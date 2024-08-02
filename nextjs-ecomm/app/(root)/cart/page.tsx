3c0c507c50557daa2e26f08999e87b668d3eef67
DFBlok <s219392862@mandela.ac.za>
2024-08-02 14:07:13 +0200

    cart detail page added

--- nextjs-ecomm/app/(root)/cart/page.tsx
+++ nextjs-ecomm/app/(root)/cart/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { getMyCart } from "@/lib/action/cart.action";
+import CartForm from "./cart-form";
+import { Cart } from "@prisma/client";
+import { CartItem } from "@/lib/types";
+import { APP_NAME } from "@/lib/constant";
 
-const ShoppingCartPage = () => {
-  return <div>ShoppingCartPage</div>;
+export const metadata = {
+  title: `Shopping Cart - ${APP_NAME}`,
 };
 
-export default ShoppingCartPage;
+type CartType = Omit<Cart, "items"> & { items: CartItem[] };
+
+const CartPage = async () => {
+  const cart = await getMyCart();
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  const parsedCart: CartType = {
+    ...cart,
+    items: cart.items,
+  };
+  return <CartForm cart={parsedCart} />;
+};
+
+export default CartPage;

b54f551b23be45386e41ba2b2d44be2300dba8fe
DFBlok <s219392862@mandela.ac.za>
2024-08-02 15:09:09 +0200

    Shipping Address function added

--- nextjs-ecomm/app/(root)/cart/cart-form.tsx
+++ nextjs-ecomm/app/(root)/cart/cart-form.tsx
@@ -126,7 +126,9 @@ export default function CartForm({ cart }: { cart?: Cart }) {
                   {formatCurrency(cart.itemsPrice)}
                 </div>
                 <Button
-                  onClick={() => startTransition(() => router.push("/signin"))}
+                  onClick={() =>
+                    startTransition(() => router.push("/shipping-address"))
+                  }
                   className="w-full"
                   disabled={isPending}
                 >
--- nextjs-ecomm/lib/types/index.ts
+++ nextjs-ecomm/lib/types/index.ts
@@ -1,6 +1,9 @@
-import { cartItemSchema } from "../validation/validator";
+import { cartItemSchema, shippingAddressSchema } from "../validation/validator";
 import { z } from "zod";
 
 //CART
 //return types objects
 export type CartItem = z.infer<typeof cartItemSchema>;
+
+// shipping address type
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
